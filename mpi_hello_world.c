6c928f8f268319c97646af2188fcd2234c4d3eff
Kyle Heaton <kyle.heaton@myresman.com>
2017-03-27 17:02:39 -0600

    created inital a, b, and c matrices

--- mpi_hello_world.c
+++ mpi_hello_world.c
@@ -1,38 +1,66 @@
-// Author: Wes Kendall
-// Copyright 2011 www.mpitutorial.com
-// This code is provided freely with the tutorials on mpitutorial.com. Feel
-// free to modify it for your own use. Any distribution of the code must
-// either provide a link to www.mpitutorial.com or keep this header intact.
-//
-// An intro MPI hello world program that uses MPI_Init, MPI_Comm_size,
-// MPI_Comm_rank, MPI_Finalize, and MPI_Get_processor_name.
-//
 #include <mpi.h>
 #include <stdio.h>
 
+#define P 2
+#define N 4
+
+double A[N][N] = { { 1.1, 3.8, 8.2, 2.9 }
+				 , { 0.4, 6.4, 2.5, 5.5 }
+				 , { 3.2, 2.2, 0.5, 2.9 }
+				 , { 0.2, 9.0, 0.6, 6,6 } };
+
+double B[N][N] = { { 7.5, 2.9, 0.2, 1.4 }
+				 , { 0.0, 2.4, 2.3, 3.6 }
+				 , { 9.2, 2.8, 0.6, 4.2 }
+				 , { 7.3, 1.2, 3.0, 2.7 } };
+
+double C[N][N] = { { 0, 0, 0, 0 }
+				 , { 0, 0, 0, 0 }
+				 , { 0, 0, 0, 0 }
+				 , { 0, 0, 0, 0 } };
+
 int main(int argc, char** argv) {
-  // Initialize the MPI environment. The two arguments to MPI Init are not
-  // currently used by MPI implementations, but are there in case future
-  // implementations might need the arguments.
+
   MPI_Init(NULL, NULL);
 
-  // Get the number of processes
+  //which processor am I?
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  // Get the rank of the process
+  //number of processors
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  // Get the name of the processor
+  //What machine?
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
   MPI_Get_processor_name(processor_name, &name_len);
 
-  // Print off a hello world message
   printf("Hello world from processor %s, rank %d out of %d processors\n",
          processor_name, world_rank, world_size);
 
-  // Finalize the MPI environment. No more MPI calls can be made after this
   MPI_Finalize();
+  /*
+  for(i = 0; i < N; i++)
+  {
+    for(j = 0; j < N; j++)
+    {
+      C[i][j] = 0;
+      for(k = 0; k < N; k++)
+      {
+        C[i][j] = A[i][k * B[i][k];
+      }
+    }
+  }
+  */
 }
+
+// Create my a, temp a
+
+
+// mpi scatter
+//mpi i send	// source, if I am 1, its 0, if I am 0 it is 1
+// mpi i receive
+//calculation 1
+// MPI wait
+// calculation 2
